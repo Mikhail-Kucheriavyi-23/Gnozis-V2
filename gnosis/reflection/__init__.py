ee02a8fd42d5540676d1b23e535033ad2afc1b52
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-17 18:33:34 +0400

    feat: export reflection runtime entry point

--- gnosis/reflection/__init__.py
+++ gnosis/reflection/__init__.py
@@ -12,6 +12,7 @@ from .analyzer import (
     ReflectionReport,
     RuleProposal,
 )
+from .runtime import reflect
 
 __all__ = [
     "CounterexampleCandidate",
@@ -20,4 +21,5 @@ __all__ = [
     "ReflectionObservation",
     "ReflectionReport",
     "RuleProposal",
+    "reflect",
 ]
