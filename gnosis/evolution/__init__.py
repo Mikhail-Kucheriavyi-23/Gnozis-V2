ab0aec70c4f8bb1064ed739238e147a18a48fb8a
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-18 04:09:19 +0400

    feat: expose provenance cross-check

--- gnosis/evolution/__init__.py
+++ gnosis/evolution/__init__.py
@@ -2,5 +2,5 @@
 from .sandbox import SandboxBudget, SandboxExecution, SandboxResult, run_sandbox
 from .evaluator import EvaluationResult, evaluate_observation
 from .promotion import PromotionCandidate, make_promotion_candidate
-from .provenance import EvidenceProvenance, build_provenance, canonical_digest, execution_id, verify_evidence_digest
+from .provenance import EvidenceProvenance, ProvenanceCrossCheck, build_provenance, canonical_digest, crosscheck_provenance, execution_id, verify_evidence_digest
 __all__ = ["SandboxBudget","SandboxExecution","SandboxResult","run_sandbox","EvaluationResult","evaluate_observation","PromotionCandidate","make_promotion_candidate"]
