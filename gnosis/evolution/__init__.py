d3b0c94c4ed26bafca69b52de085d269a4fe856e
Mikhail-Kucheriavyi-23 <qwery230992.kmr@gmail.com>
2026-09-18 04:16:00 +0400

    feat: expose evolution replay contracts

--- gnosis/evolution/__init__.py
+++ gnosis/evolution/__init__.py
@@ -2,5 +2,6 @@
 from .sandbox import SandboxBudget, SandboxExecution, SandboxResult, run_sandbox
 from .evaluator import EvaluationResult, evaluate_observation
 from .promotion import PromotionCandidate, PromotionGate, evaluate_promotion_gate, make_promotion_candidate
+from .replay import ReplayResult, replay_evidence, replay_identity
 from .provenance import EvidenceProvenance, ProvenanceCrossCheck, build_provenance, canonical_digest, crosscheck_provenance, execution_id, verify_evidence_digest
-__all__ = ["SandboxBudget","SandboxExecution","SandboxResult","run_sandbox","EvaluationResult","evaluate_observation","PromotionCandidate","PromotionGate","evaluate_promotion_gate","make_promotion_candidate"]
+__all__ = ["SandboxBudget","SandboxExecution","SandboxResult","run_sandbox","EvaluationResult","evaluate_observation","PromotionCandidate","PromotionGate","ReplayResult","replay_evidence","replay_identity","evaluate_promotion_gate","make_promotion_candidate"]
